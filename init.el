cfa93a4a07397234874849b286c4721e3bd0fb8f
Yuriy VG <yuravg@gmail.com>
2019-08-02 13:16:06 +0300

    Add settings for pylint(Python checker).

--- init.el
+++ init.el
@@ -168,6 +168,7 @@ need Org version to be at least 9.x.")
     ;; projectile ; Better than fiplr < fork
 
     ;; YuraVG:
+    pylint
     autopair
     aggressive-indent
     google-translate
--- setup-files/setup-flycheck.el
+++ setup-files/setup-flycheck.el
@@ -36,7 +36,25 @@
     (setq flycheck-checker-error-threshold 800)
 
     ;; Enable C++ exceptions
-    (add-to-list 'flycheck-clang-args "-fcxx-exceptions")))
+    (add-to-list 'flycheck-clang-args "-fcxx-exceptions")
+
+    ;;; Python checkers
+    (use-package pylint
+      :config
+      ;; Messages:
+      ;; http://stackoverflow.com/questions/4341746/how-do-i-disable-a-pylint-warning
+      ;; http://pylint-messages.wikidot.com/all-messages
+      ;; http://pylint.readthedocs.io/en/1.6.0/features.html
+      (progn
+        (autoload 'pylint "pylint")
+        (add-hook 'python-mode-hook #'pylint-add-menu-items)
+        (add-hook 'python-mode-hook #'pylint-add-key-bindings)
+
+        ;; Enable pylint with flycheck:
+        (add-to-list 'flycheck-checkers 'python-pylint)
+        ;; simultaneous launch checkers:
+        ;; https://github.com/flycheck/flycheck/issues/186
+        (flycheck-add-next-checker 'python-flake8 'python-pylint)))))
 
 
 (provide 'setup-flycheck)

3618f85675830c9eb00981c99095fa328f8c3fdf
Yuriy VG <yuravg@gmail.com>
2019-08-05 14:23:49 +0300

    Rollback setup-server.el
    This version (after rollback) allow open file from shell by command:
    "em <file_name>"
    
    Where 'em' this is executed shell script, with content:
        if [ -s ~/.emacs.d/server*/server ]; then
            emacsclient -n -q "$@" 2> /dev/null &
        else
            if [ "$OS" = "Windows_NT" ]; then
                runemacs.exe "$@" &
            else
                emacs "$@" &
            fi
        fi
    
    I do not want to give up the ability to open a file from the command line.
    May be there is another way to open files from the command line,
    but I can't find them and I make rollback.

--- setup-files/setup-server.el
+++ setup-files/setup-server.el
@@ -1,69 +1,33 @@
-;; Time-stamp: <2018-02-20 15:08:17 kmodi>
+;; Time-stamp: <2016-11-20 22:24:32 kmodi>
 
 ;; server/daemon setup
 
 (use-package server
   :init
   (progn
-    (when (equal window-system 'w32)
-      (setq server-use-tcp t)
-      ;; `server-auth-dir' is used for server authentication files only if
-      ;; `server-use-tcp' is non-nil.
-      ;; On Windows, also set the EMACS_SERVER_FILE environment variable to
-      ;; point to the `server' file. For example, for emacs 25.1, that location
-      ;; would be "PATH\TO\.emacs.d\server_25_1_HOSTNAME\server".
-
-      ;; Append the machine name to `server-auth-dir' to prevent server file
-      ;; clashes when the same emacs config is shared simultaneously across
-      ;; different machines (e.g. via Dropbox).
-      (setq server-auth-dir (let* ((machine-name (>=e "25.0"
-                                                     (system-name)
-                                                   system-name))
-                                   (server-dir (format "server_%s_%s"
-                                                       emacs-version-short
-                                                       machine-name))
-                                   (dir (file-name-as-directory (expand-file-name server-dir user-emacs-directory))))
-                              (make-directory dir :parents)
-                              dir))))
+    (setq server-use-tcp t)
+    ;; `server-auth-dir' is used for server authentication files only if
+    ;; `server-use-tcp' is non-nil.
+    ;; On Windows, also set the EMACS_SERVER_FILE environment variable to
+    ;; point to the `server' file. For example, for emacs 25.1, that location
+    ;; would be "PATH\TO\.emacs.d\server_25_1_HOSTNAME\server".
+    (setq server-auth-dir
+          (let ((dir (concat user-emacs-directory
+                             "server"
+                             "/"))) ; must end with /
+            (make-directory dir :parents)
+            dir)))
   :config
   (progn
-    (when (equal window-system 'w32)
-      ;; http://stackoverflow.com/a/1566618/1219634
-      ;; Suppress error "directory  ~/.emacs.d/server is unsafe". It is needed
-      ;; for the server to start on Windows.
-      ;; Below hack should not be needed if the User is the Owner of the
-      ;; `server-auth-dir'. To check that, Right-click on that server directory
-      ;; > Properties > Advanced > Owner, and check that the owner is the User
-      ;; and not the Administrator. If that's not the case and you do not have
-      ;; admin rights to fix that, below hack just works.
-      (defun server-ensure-safe-dir (dir) "Noop" t)
-
-      ;; http://www.emacswiki.org/emacs/Edit_with_Emacs
-      ;; `edit-server' package to edit the text boxes in Chrome browser using
-      ;; emacsclient
-      (use-package edit-server
-        :ensure t
-        :config
-        (progn
-          ;; Don't pop up a new frame
-          (setq edit-server-new-frame nil)
-          (setq edit-server-default-major-mode 'org-mode)
-          (setq edit-server-url-major-mode-alist
-                '(("github\\.com" . markdown-mode)
-                  ("stackexchange\\.com" . markdown-mode)
-                  ("stackoverflow\\.com" . markdown-mode)
-                  ("reddit\\.com" . markdown-mode)))
-
-          ;; Integration with Gmail
-          (use-package edit-server-htmlize
-            :ensure t
-            :defer t
-            :config
-            (progn
-              (add-hook 'edit-server-start-hook #'edit-server-maybe-dehtmlize-buffer)
-              (add-hook 'edit-server-done-hook  #'edit-server-maybe-htmlize-buffer)))
-
-          (edit-server-start))))
+    ;; http://stackoverflow.com/a/1566618/1219634
+    ;; Suppress error "directory  ~/.emacs.d/server is unsafe". It is needed
+    ;; for the server to start on Windows.
+    ;; Below hack should not be needed if the User is the Owner of the
+    ;; `server-auth-dir'. To check that, Right-click on that server directory
+    ;; > Properties > Advanced > Owner, and check that the owner is the User
+    ;; and not the Administrator. If that's not the case and you do not have
+    ;; admin rights to fix that, below hack just works.
+    (defun server-ensure-safe-dir (dir) "Noop" t)
 
     ;; Start a server if (server-running-p) does not return t (e.g. if it
     ;; returns nil or :other)

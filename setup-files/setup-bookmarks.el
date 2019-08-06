2918a33e16dac771f2e4a2ef38689aababcb8ccb
Yuriy VG <yuravg@gmail.com>
2019-08-06 11:54:49 +0300

    Change hydra-bm(bookmark): header, behavior, binds, add commands.

--- setup-files/setup-bookmarks.el
+++ setup-files/setup-bookmarks.el
@@ -92,27 +92,30 @@
             (deactivate-mark))
         (bm-bookmark-regexp)))
 
-    (defhydra hydra-bm (:color pink
+    (defhydra hydra-bm (:color red
                         :hint nil
                         :body-pre (when (not (use-region-p)) (push-mark)))
       "
-Bookmark _n_ext (_N_ in lifo order)            toggle book_m_ark        ^^_/_ bm lines matching regexp                          toggle per_s_istence
-         _p_revious (_P_ in lifo order)        _a_nnotate               _x_/_X_ remove all bm from current/all buffer(s)        _r_eturn to from where you started
-    "
+Bookmarks
+  _n_ext (_N_ in lifo order)         toggle book_m_ark     ^^_/_ bm lines matching regexp                       _t_oggle persistence                    List: _s_et, _j_ump, _l_ist
+  _p_revious (_P_ in lifo order)     _a_nnotate            _x_/_X_ remove all bm from current/all buffer(s)     _r_eturn to from where you started
+"
       ("m"   bm-toggle)
-      ("M"   bm-toggle :color blue)
-      ("a"   bm-bookmark-annotate :color blue)
+      ("M"   bm-toggle)
+      ("a"   bm-bookmark-annotate)
       ("n"   bm-common-next)
       ("N"   bm-lifo-next)
       ("p"   bm-common-previous)
       ("P"   bm-lifo-previous)
-      ("/"   modi/bm-bookmark-regexp :color blue)
-      ("s"   bm-toggle-buffer-persistence)
-      ("x"   bm-remove-all-current-buffer :color blue)
-      ("X"   bm-remove-all-all-buffers :color blue)
-      ("r"   pop-to-mark-command :color blue)
-      ("RET" nil "cancel" :color blue)
-      ("q"   nil "cancel" :color blue))))
+      ("/"   modi/bm-bookmark-regexp)
+      ("t"   bm-toggle-buffer-persistence)
+      ("x"   bm-remove-all-current-buffer)
+      ("X"   bm-remove-all-all-buffers)
+      ("r"   pop-to-mark-command)
+      ("s"   bookmark-set :exit t)
+      ("j"   counsel-bookmark :exit t)
+      ("l"   bookmark-bmenu-list :exit t)
+      ("q"   nil "cancel"))))
 
 ;;; bookmark+ (bmkp)
 ;; https://www.emacswiki.org/emacs/BookmarkPlus

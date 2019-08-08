c0c178bd0f18c7b631dbe0a8859efb7829d7e2f8
Yuriy VG <yuravg@gmail.com>
2019-08-08 12:30:47 +0300

    Delete ido customization which unnecessary for yasnippet and magit
    Use Ido for:
    - yasnippet: `yas-ido-expand'
    - magit: `magit-branch-checkout', `magit-log-buffer-file'.

--- setup-files/setup-ido.el
+++ setup-files/setup-ido.el
@@ -15,21 +15,21 @@
     (defvar ido-cur-list               nil)
     (defvar ido-context-switch-command nil))
   :defer t
-  :init
-  (progn
-    (setq ido-enable-flex-matching  t) ; enable fuzzy search
-    (setq ido-everywhere            t)
-    (setq ido-create-new-buffer     'always) ; create a new buffer if no buffer matches substring
-    (setq ido-file-extensions-order '(".sv" ".v" ".svh" ".tv" ".m" ".c" ".cpp" ".el"))
-    ;;  customize the order in which files are sorted when Ido displays them in
-    ;; the minibuffer. There are certain file extensions I use more than others,
-    ;; so I tell Ido to emphasize those
-    (setq ido-use-filename-at-point 'guess) ; find file at point using ido
-    ;; look into other directories if the entered filename doesn't exist
-    ;; in current directory ido-auto-merge-work-directories-length -1
-    ;; do NOT look into other directories if the entered filename doesn't
-    ;; exist in current directory
-    (setq ido-auto-merge-work-directories-length 0))
+  ;; :init
+  ;; (progn
+  ;;   (setq ido-enable-flex-matching  t) ; enable fuzzy search
+  ;;   (setq ido-everywhere            t)
+  ;;   (setq ido-create-new-buffer     'always) ; create a new buffer if no buffer matches substring
+  ;;   (setq ido-file-extensions-order '(".sv" ".v" ".svh" ".tv" ".m" ".c" ".cpp" ".el"))
+  ;;   ;;  customize the order in which files are sorted when Ido displays them in
+  ;;   ;; the minibuffer. There are certain file extensions I use more than others,
+  ;;   ;; so I tell Ido to emphasize those
+  ;;   (setq ido-use-filename-at-point 'guess) ; find file at point using ido
+  ;;   ;; look into other directories if the entered filename doesn't exist
+  ;;   ;; in current directory ido-auto-merge-work-directories-length -1
+  ;;   ;; do NOT look into other directories if the entered filename doesn't
+  ;;   ;; exist in current directory
+  ;;   (setq ido-auto-merge-work-directories-length 0))
   :config
   (progn
     (ido-mode 1)
@@ -52,62 +52,6 @@
       :config
       (progn
         (ido-vertical-mode 1))) ; flx-ido looks better with ido-vertical-mode
-
-    (use-package ido-ubiquitous
-      :ensure t
-      :preface
-      (progn
-        (defvar ido-ubiquitous-debug-mode nil))
-      :config
-      (progn
-        (ido-ubiquitous-mode 1)))
-
-    ;; Sometimes when using ido-switch-buffer the *Messages* buffer get in the
-    ;; way, so we set it to be ignored (it can be accessed using `C-h e', so
-    ;; there is really no need for it in the buffer list).
-    ;;   Also left mouse click in the echo area brings up the *Messages* buffer.
-    ;; https://github.com/larstvei/dot-emacs
-    (add-to-list 'ido-ignore-buffers "*Messages*")
-
-    ;; Sort ido filelist by mtime instead of alphabetically
-    ;; http://www.emacswiki.org/emacs/InteractivelyDoThings
-    (defun modi/ido-sort-mtime ()
-      ;; Sort the list only when the File gid is non-zero;
-      ;; otherwise the sort function errors out.
-      ;; (message "Current dir: %s File GID: %s File Attributes: %s"
-      ;;          ido-current-directory
-      ;;          (cl-fourth (file-attributes ido-current-directory))
-      ;;          (file-attributes ido-current-directory))
-      (when (file-exists-p ido-current-directory) ; only if the current directory exists
-        (when (not (= (cl-fourth (file-attributes ido-current-directory)) 0))
-          (setq ido-temp-list
-                (sort ido-temp-list
-                      (lambda (a b)
-                        (time-less-p
-                         (cl-sixth (file-attributes (concat ido-current-directory b)))
-                         (cl-sixth (file-attributes (concat ido-current-directory a))))))))
-        (ido-to-end  ; move . files to end (again)
-         (delq nil (mapcar
-                    (lambda (x) (and (char-equal (string-to-char x) ?.) x))
-                    ido-temp-list)))))
-    (add-hook 'ido-make-file-list-hook #'modi/ido-sort-mtime)
-    (add-hook 'ido-make-dir-list-hook  #'modi/ido-sort-mtime)
-
-    ;; Open recent files with IDO,
-    ;; https://github.com/lunaryorn/stante-pede/blob/master/init.el
-    ;; http://emacsredux.com/blog/2013/04/05/recently-visited-files/
-    ;; http://www.reddit.com/r/emacs/comments/21a4p9/use_recentf_and_ido_together/cgc8f6b
-    ;; `abbreviate-file-name' abbreviates home dir to ~/ in the file list
-    ;; Custom abbreviations can be added to `directory-abbrev-alist'.
-    (with-eval-after-load 'recentf
-      (defun ido-find-recentf ()
-        "Use ido to select a recently opened file from the `recentf-list'"
-        (interactive)
-        (find-file
-         (ido-completing-read "Recentf open: "
-                              (mapcar 'abbreviate-file-name recentf-list)
-                              nil t)))
-      (bind-key "M-o" #'ido-find-recentf))
 
     (defun endless/ido-bury-buffer-at-head ()
       "Bury the buffer at the head of `ido-matches'.
@@ -143,10 +87,7 @@ This is merged into emacs 25.0."
        ("C-f"    . ido-magic-forward-char)
        ("C-b"    . ido-magic-backward-char)
        ("C-i"    . ido-toggle-ignore)
-       ("C-j"    . ido-exit-minibuffer))
-      (>=e "25.0"
-          (bind-key "C-S-b" #'ido-bury-buffer-at-head ido-completion-map) ; emacs >= 25.0
-        (bind-key "C-S-b" #'endless/ido-bury-buffer-at-head ido-completion-map))) ; emacs < 25.0
+       ("C-j"    . ido-exit-minibuffer)))
     (add-hook 'ido-setup-hook #'ido-define-keys)))
 
 

99a61c63a410c0714a39999de610b4c886eb883d
Yuriy VG <yuravg@gmail.com>
2020-01-11 00:11:50 +0300

    Fix google-translate
    Google-translate abort with error: (args-out-of-range [] 1)
    
    https://github.com/atykhonov/google-translate/issues/98#issuecomment-562870854.

--- setup-files/setup-google-translate.el
+++ setup-files/setup-google-translate.el
@@ -17,7 +17,19 @@
         (defalias 'gtq 'google-translate-query-translate)
         (defalias 'gtqr 'google-translate-query-translate-reverse
           ;; replacement of doc-string
-          "Revert interactively translate text with Google Translate")))))
+          "Revert interactively translate text with Google Translate")
+
+        ;; https://github.com/atykhonov/google-translate/issues/98#issuecomment-562870854
+        ;; Debugger entered--Lisp error: (args-out-of-range [] 1)
+        (defun google-translate-json-suggestion (json)
+          "Retrieve from JSON (which returns by the
+`google-translate-request' function) suggestion. This function
+does matter when translating misspelled word. So instead of
+translation it is possible to get suggestion."
+          (let ((info (aref json 7)))
+            (if (and info (> (length info) 0))
+                (aref info 1)
+              nil)))))))
 
 
 (provide 'setup-google-translate)

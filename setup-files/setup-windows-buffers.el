4fc074a985897d12285c8aa63e74759ae8bf38fb
Yuriy VG <yuravg@gmail.com>
2021-05-24 11:01:21 +0300

    Fix typo in 'dos-file' command.

--- setup-files/setup-windows-buffers.el
+++ setup-files/setup-windows-buffers.el
@@ -147,7 +147,7 @@ Prefixed FULL-PATH with \\[universal-argument], expand the file name to its full
 (defun dos-file ()
   "Change the current buffer to Latin 1 with DOS line-ends."
   (interactive)
-  (set-buffer-file-coding-system 'iso-latin-1-dos :forece))
+  (set-buffer-file-coding-system 'iso-latin-1-dos :force))
 (defun mac-file ()
   "Change the current buffer to Latin 1 with Mac line-ends."
   (interactive)

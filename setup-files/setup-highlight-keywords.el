77ca947d56c65440dcfc4e4f0bfb8216958a9c9f
Yuriy VG <yuravg@gmail.com>
2022-04-04 09:55:16 +0300

    Highlight 'pragma' keyword for Verilog mode.

--- setup-files/setup-highlight-keywords.el
+++ setup-files/setup-highlight-keywords.el
@@ -248,7 +248,7 @@
 ;;;; Preprocessor
 (font-lock-add-keywords
  'verilog-mode
- '(("\\<\\(\\(synopsys\\|synthesis\\) translate_\\(off\\|on\\)\\)\\>" 1 'font-lock-preprocessor-face prepend)
+ '(("\\<\\(\\(synopsys\\|synthesis\\|pragma\\) translate_\\(off\\|on\\)\\)\\>" 1 'font-lock-preprocessor-face prepend)
    ("\\(altera \\(message_off\\|message_on\\) [0-9]\\{5,6\\}\\)" 1 'font-lock-preprocessor-face prepend)))
 
 ;;;; Identifier

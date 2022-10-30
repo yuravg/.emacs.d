d3f3b289d7072491a511403d0fcc80369bbbaaec
Yuriy VG <yuravg@gmail.com>
2022-10-30 17:05:41 +0300

    Improve make-file mode selection mask
    Exclude Perl make-file: Makefile.PL.

--- setup-files/setup-makefile.el
+++ setup-files/setup-makefile.el
@@ -5,10 +5,10 @@
 (use-package make-mode
   :mode (("\\makefile\\'"        . makefile-mode)
          ("\\Makefile\\'"        . makefile-mode)
-         ("\\Makefile\..+\\'"    . makefile-mode)
          ("\\Make.defines\\'"    . makefile-mode)
          ("\\Make.defines.in\\'" . makefile-mode)
-         ("\\.inc\\'"            . makefile-mode))
+         ("\\.inc\\'"            . makefile-mode)
+         ("\\Makefile\.[^P][^L].+\\'" . makefile-mode))
   :mode (("\\GNUmakefile\\'" . makefile-gmake-mode ))
   :bind (:map makefile-mode-map
          ("C-j" . newline))  ;; Override the default binding to `compile-goto-error', compile.el

f78e1e6a08c09d358d4765774da561c25a704f73
Yuriy VG <yuravg@gmail.com>
2025-01-31 13:39:22 +0300

    Edit PlantUML mode indentation
    Use 2 spaces for indentation instead of 8 spaces.

--- setup-files/setup-plantuml.el
+++ setup-files/setup-plantuml.el
@@ -19,6 +19,12 @@
     (setq plantuml-default-exec-mode 'jar)
     ;; Set path to my local plantuml.jar file
     (setq plantuml-jar-path org-plantuml-jar-path)
+    (defun yura/plantuml-set-indentaion ()
+      "Customize the indentation for `plantuml-mode'."
+      (setq tab-width 2
+            plantuml-indent-level 2
+            indent-tabs-mode nil))
+    (add-hook 'plantuml-mode-hook #'yura/plantuml-set-indentaion)
 
     ;; JSON
     (use-package json-mode

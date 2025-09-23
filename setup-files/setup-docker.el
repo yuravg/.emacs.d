1821d1c6609769076fe931a01dd2d835f72c48a0
Yuriy VG <yuravg@gmail.com>
2025-09-23 09:58:07 +0300

    Add tips for Docker mode.

--- setup-files/setup-docker.el
+++ setup-files/setup-docker.el
@@ -8,5 +8,21 @@
 (use-package docker
   :ensure t)
 
-
 (provide 'setup-docker)
+
+;; TIPS
+;; |---------+----------------------|
+;; | Binding | Description          |
+;; |---------+----------------------|
+;; | ?       | List actions         |
+;; | l       | Configure listing    |
+;; | m       | Mark item            |
+;; | u       | Unmark item          |
+;; | t       | Toggle marks         |
+;; | U       | Unmark all           |
+;; | s       | Sort                 |
+;; | * r     | Mark items by regexp |
+;; | <       | Shrink column        |
+;; | >       | Enlarge column       |
+;; | C-c C-e | Export to csv        |
+;; |---------+----------------------|

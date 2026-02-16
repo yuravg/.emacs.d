8b7fbc5a068812d20424df77c2d1f6f2658fed3a
Yuriy VG <yuravg@gmail.com>
2026-02-16 16:44:13 +0300

    Support image zoom for PlantUML preview buffers.
    Use image-increase-size/image-decrease-size for non-file
    images (overlays) while falling back to image-transform-set-scale for
    standard image-mode buffers.  This fixes zoom functionality for PlantUML
    preview buffers.

--- setup-files/setup-image.el
+++ setup-files/setup-image.el
@@ -1,9 +1,28 @@
-;; Time-stamp: <2019-07-30 14:45:04 kmodi>
+;; Time-stamp: <2026-02-16 16:35:45 gritsenko>
 
 (use-package image-mode
   :defer t
   :bind
   (:map image-mode-map
-   ("C-c C-h" . my/plantuml-toggle-source-diagram)))
+   ("C-c C-h" . my/plantuml-toggle-source-diagram)
+   ("C-+"     . my/image-zoom-in)
+   ("C-="     . my/image-zoom-in)   ; alternative, easier on some keyboards
+   ("C--"     . my/image-zoom-out)
+   ("C-_"     . my/image-zoom-out)) ; fallback for C--
+  :config
+  (progn
+    (defun my/image-zoom-in ()
+      "Zoom in on image at point. Works for both file-backed and non-file images."
+      (interactive)
+      (if (image-at-point-p)
+          (image-increase-size 0.2)
+        (image-transform-set-scale 1.2)))
+
+    (defun my/image-zoom-out ()
+      "Zoom out on image at point. Works for both file-backed and non-file images."
+      (interactive)
+      (if (image-at-point-p)
+          (image-decrease-size 0.2)
+        (image-transform-set-scale (/ 1.0 1.2))))))
 
 (provide 'setup-image)

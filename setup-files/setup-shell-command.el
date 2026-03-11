9ce2618e8cbe58435169431add22821f94bf35a4
Yuriy VG <yuravg@gmail.com>
2026-03-11 16:02:13 +0300

    Add commands for shell buffers
    Add commands:
    - Open a new shell buffer and execute user command
    - Exit and Kill the shell buffer, bind: 'C-c C-q'.

--- setup-files/setup-shell-command.el
+++ setup-files/setup-shell-command.el
@@ -1,10 +1,37 @@
 
-;; Shell-command
+;; Shell Command Buffers
 
 ;; Contents:
 ;;
+;;    my/shell-run-command
+;;    my/shell-exit-and-kill
 ;;    Run current buffer in shell
 ;;    Package Comint
+
+
+;;;; my/shell-run-command
+(defun my/shell-run-command (command)
+  "Open a new shell buffer and execute COMMAND in it.
+  The buffer gets a unique name based on the command."
+  (interactive (list (read-shell-command "Shell command: ")))
+  (let* ((buf-name (generate-new-buffer-name
+                    (format "*shell: %s*" (truncate-string-to-width command 30))))
+         (buf (shell buf-name)))
+    ;; Wait for shell prompt before sending command
+    (set-process-query-on-exit-flag (get-buffer-process buf) nil)
+    (comint-send-string (get-buffer-process buf) (concat command "\n"))))
+
+;;;; my/shell-exit-and-kill
+(defun my/shell-exit-and-kill ()
+  "Send `exit' to the shell process and kill the buffer when it finishes."
+  (interactive)
+  (when-let* ((proc (get-buffer-process (current-buffer))))
+    (set-process-sentinel proc
+                          (lambda (process _event)
+                            (when (memq (process-status process) '(exit signal))
+                              (kill-buffer (process-buffer process)))))
+    (comint-send-string proc "exit\n")))
+(bind-key "C-c C-q" #'my/shell-exit-and-kill shell-mode-map)
 
 ;;;; Run current buffer in shell
 (defun run-buffer-in-shell ()
